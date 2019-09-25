6614e04836d0e00bcf4b44d0c2b557b74b961a24
neil3d <neil3d@126.com>
2019-09-25 20:27:45 +0800

    thumbnail checkerboard background

--- Plugins/AnimatedTexture/Source/AnimatedTextureEditor/Private/AnimatedTextureThumbnailRenderer.cpp
+++ Plugins/AnimatedTexture/Source/AnimatedTextureEditor/Private/AnimatedTextureThumbnailRenderer.cpp
@@ -2,7 +2,10 @@
 #include "AnimatedTextureThumbnailRenderer.h"
 #include "AnimatedTexture2D.h"
 
+#include "CanvasTypes.h"	// Engine
 #include "CanvasItem.h"	// Engine
+#include "Engine/Texture2D.h"	// Engine
+#include "ThumbnailRendering/ThumbnailManager.h"	// UnrealEd
 
 void UAnimatedTextureThumbnailRenderer::GetThumbnailSize(UObject* Object, float Zoom, uint32& OutWidth, uint32& OutHeight) const
 {
@@ -24,10 +27,20 @@ void UAnimatedTextureThumbnailRenderer::Draw(UObject * Object, int32 X, int32 Y,
 	UAnimatedTexture2D* Texture = Cast<UAnimatedTexture2D>(Object);
 	if (Texture != nullptr && Texture->Resource != nullptr)
 	{
+		if (Texture->SupportsTransparency)
+		{
+			// If support transparentcy, draw a checkerboard background first.
+			const int32 CheckerDensity = 8;
+			UTexture2D* Checker = UThumbnailManager::Get().CheckerboardTexture;
+			Canvas->DrawTile(
+				0.0f, 0.0f, Width, Height,							// Dimensions
+				0.0f, 0.0f, CheckerDensity, CheckerDensity,			// UVs
+				FLinearColor::White, Checker->Resource);			// Tint & Texture
+		}
+
 		// Use A canvas tile item to draw
 		FCanvasTileItem CanvasTile(FVector2D(X, Y), Texture->Resource, FVector2D(Width, Height), FLinearColor::White);
 		CanvasTile.BlendMode = Texture->SupportsTransparency ? SE_BLEND_Translucent : SE_BLEND_Opaque;
-		//CanvasTile.BatchedElementParameters = BatchedElementParameters;
 		CanvasTile.Draw(Canvas);
 
 	}// end of if(texture is valid)
