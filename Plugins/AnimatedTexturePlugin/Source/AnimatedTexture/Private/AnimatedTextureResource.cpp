394adcb20594a559dac7a610653fd878c54e34f0
neil3d <neil3d@126.com>
2021-12-18 10:10:13 +0800

    fix RHI textrue create info

--- Plugins/AnimatedTexturePlugin/Source/AnimatedTexture/Private/AnimatedTextureResource.cpp
+++ Plugins/AnimatedTexturePlugin/Source/AnimatedTexture/Private/AnimatedTextureResource.cpp
@@ -6,7 +6,7 @@
 #include "DeviceProfiles/DeviceProfile.h"	// Engine
 #include "DeviceProfiles/DeviceProfileManager.h"	// Engine
 
-FAnimatedTextureResource::FAnimatedTextureResource(UAnimatedTexture2D * InOwner) :Owner(InOwner)
+FAnimatedTextureResource::FAnimatedTextureResource(UAnimatedTexture2D* InOwner) :Owner(InOwner)
 {
 }
 
@@ -24,21 +24,54 @@ uint32 FAnimatedTextureResource::GetSizeY() const
 	return 2;
 }
 
+static ESamplerAddressMode _ConvertAddressMode(enum TextureAddress addr)
+{
+	ESamplerAddressMode ret = AM_Wrap;
+	switch (addr)
+	{
+	case TA_Wrap:
+		ret = AM_Wrap;
+		break;
+	case TA_Clamp:
+		ret = AM_Clamp;
+		break;
+	case TA_Mirror:
+		ret = AM_Mirror;
+		break;
+	}
+	return ret;
+}
+
 void FAnimatedTextureResource::InitRHI()
 {
-	//-- create FSamplerStateRHIRef FTexture::SamplerStateRHI
-	CreateSamplerStates(
-		GetDefaultMipMapBias()
+	// Create the sampler state RHI resource.
+	ESamplerAddressMode AddressU = _ConvertAddressMode(Owner->AddressX);
+	ESamplerAddressMode AddressV = _ConvertAddressMode(Owner->AddressY);
+	ESamplerAddressMode AddressW = AM_Wrap;
+
+	FSamplerStateInitializerRHI SamplerStateInitializer
+	(
+		(ESamplerFilter)UDeviceProfileManager::Get().GetActiveProfile()->GetTextureLODSettings()->GetSamplerFilter(Owner),
+		AddressU,
+		AddressV,
+		AddressW
 	);
+	SamplerStateRHI = GetOrCreateSamplerState(SamplerStateInitializer);
 
-	//-- create FTextureRHIRef FTexture::TextureRHI
-	const ETextureCreateFlags CreateFlags = TexCreate_Dynamic | TexCreate_ShaderResource | TexCreate_CPUWritable | TexCreate_DisableDCC;
+	ETextureCreateFlags  Flags = TexCreate_None;
+	if (!Owner->SRGB)
+		bIgnoreGammaConversions = true;
+
+	if (Owner->SRGB)
+		Flags |= TexCreate_SRGB;
+	if (Owner->bNoTiling)
+		Flags |= TexCreate_NoTiling;
+
 	uint32 NumMips = 1;
-
-	FRHIResourceCreateInfo CreateInfo;
-	TextureRHI = RHICreateTexture2D(GetSizeX(), GetSizeY(), PF_B8G8R8A8, NumMips, 1, CreateFlags, CreateInfo);
+	FString Name = Owner->GetName();
+	FRHIResourceCreateInfo CreateInfo(*Name);
+	TextureRHI = RHICreateTexture2D(GetSizeX(), GetSizeY(), PF_B8G8R8A8, NumMips, 1, Flags, CreateInfo);
 	TextureRHI->SetName(Owner->GetFName());
-
 	RHIUpdateTextureReference(Owner->TextureReference.TextureReferenceRHI, TextureRHI);
 
 	if (Owner)
